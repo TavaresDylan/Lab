9125de606ca1c7c9d6de612699148ea75f3a1ce9
DylanTavares <tavares.dylan03@gmail.com>
2024-06-10 15:59:00 +0200

    feat: regex split line data

--- BotRequestBlacklist/src/main.ts
+++ BotRequestBlacklist/src/main.ts
@@ -1,27 +1,66 @@
 import * as fs from "node:fs";
 
+interface Log {
+    date: string;
+    ip: string;
+    method: string;
+    path: string;
+    statusCode: string;
+}
+
 class LogParser {
-    logLines: string[];
+    public logLines: string[];
+    public parsedLog: Log[];
 
     constructor() {
         this.logLines = []
+        this.parsedLog = []
     }
 
-    public parse(filePath: string) {
+    public parseFile(filePath: string) {
         fs.readFile(filePath, "utf8", (err, file) => {
             if(err){
                 console.error(err);
             } else {
                 this.logLines = file.split('\n');
-                console.log(this.logLines.slice(0,10))
+                // console.log(this.logLines.slice(0,15));
+                this.parseLines(this.logLines);
             }
         });
-        return this.logLines
-    }
+    };
+
+    /*
+    * @description Parse an array of string in the form of => `date - ip : Method path`
+    * @param lines string[]
+     */
+    public parseLines(lines: string[]) {
+        lines.slice(0,2).forEach((line) => {
+            const dateAndTimeRegex: RegExp = new RegExp("(?<=\\[Last Sync: )(.*?)(?=\\])");
+            const dateRegex: RegExp = new RegExp("(?<=\\[Last Sync: )(.*?)(?= @)");
+            const timeRegex: RegExp = new RegExp("(?<=@ )(.*?)(?=\\])");
+            const ipRegex: RegExp = new RegExp("(?:\\d{1,3}\\.){3}\\d{1,3}");
+            const pathRegex: RegExp = new RegExp("(?<= )(?=\\/)(.*)(?= -)");
+            const httpMethodRegex: RegExp = new RegExp("(?<=(?:\\d{1,3}\\.){3}\\d{1,3}: )(.*)(?= \\/)");
+            const statusCodeRegex: RegExp = new RegExp("(?<=\\> )(.*)(?=)");
+
+            const dateAndTime = line.match(dateAndTimeRegex);
+            console.log("Date and time :", dateAndTime);
+            const date = line.match(dateRegex);
+            console.log("Date :", date);
+            const time = line.match(timeRegex);
+            console.log("Time :", time);
+            const ip = line.match(ipRegex);
+            console.log("Ip :", ip);
+            const path = line.match(pathRegex);
+            console.log("Path :", path);
+            const httpMethod = line.match(httpMethodRegex);
+            console.log("Http method :", httpMethod);
+            const statusCode = line.match(statusCodeRegex)
+            console.log("Status code :", statusCode);
+        })
+    };
 }
 
 const logParser = new LogParser();
+logParser.parseFile('src/logs/requests.log');
 
-const res =logParser.parse('src/logs/requests.log');
-
-console.log(res.slice(0,15));
