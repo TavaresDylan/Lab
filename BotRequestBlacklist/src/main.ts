092eabdf8d8dafee005b0bfa0159e0e80f6a7a13
DylanTavares <tavares.dylan03@gmail.com>
2024-06-11 11:41:35 +0200

    refactor: .map replaced by .forEach & doc annotations added on functions

--- BotRequestBlacklist/src/main.ts
+++ BotRequestBlacklist/src/main.ts
@@ -2,6 +2,7 @@ import * as fs from "node:fs";
 
 interface Log {
     date: string;
+    time: string;
     ip: string;
     method: string;
     path: string;
@@ -22,13 +23,14 @@ class LogParser {
     constructor(forbiddenWords: string[] = [".php"], ignoredIps: string[] = ["127.0.0.1"], blacklistPath?: string) {
         this.ignoredIps = ignoredIps;
         this.blacklist = [];
-        this.blacklistPath = blacklistPath
+        this.blacklistPath = blacklistPath;
         this.forbiddenWords = forbiddenWords;
     }
 
-    /*
-    * @description Parse an array of string in the form of => `date - ip : Method path`
-    * @param lines string[]
+    /**
+     * @desc - Parse an Argolight API .log file into a JSON
+     * @param {string[]} lines - The lines of the .log
+     * @return {Log[]} - Returns an array of parsed logs
      */
     public parseLines(lines: string[]): Log[] {
         const parsedLogs: Log[] = []
@@ -48,15 +50,20 @@ class LogParser {
             const statusCode = line.match(statusCodeRegex);
 
             if (date !== null && time !== null && ip !== null && path !== null && httpMethod !== null && statusCode !== null) {
-                parsedLogs.push({"date": date[0].toString(), "ip": ip[0].toString(), "method": httpMethod[0].toString(), "path": path[0].toString(), "statusCode": statusCode[0].toString()});
+                parsedLogs.push({"date": date[0].toString(), time: time[0].toString(), "ip": ip[0].toString(), "method": httpMethod[0].toString(), "path": path[0].toString(), "statusCode": statusCode[0].toString()});
             }
         })
         return parsedLogs;
     };
 
+    /**
+     * @desc - Filters the passed parsedLogs and return the total count of distinct ips
+     * @param {Log[]} parsedLogs - The parsedLogs to count the total of distinct ips
+     * @return {number} - Returns the count of unique ips found into the passed parsed logs array
+     */
     public countDistinctIps(parsedLogs: Log[]): number {
         let uniqueIps: string[] = [];
-        parsedLogs.map((logLine) => {
+        parsedLogs.forEach((logLine) => {
             if(!uniqueIps.includes(logLine.ip) && !this.ignoredIps.includes(logLine.ip)){
                 uniqueIps.push(logLine.ip);
             }
@@ -64,9 +71,14 @@ class LogParser {
         return uniqueIps.length;
     }
 
-    private filterUniqueIps(parsedLogs: Log[]): string[] {
+    /**
+     * @desc - Filters the passed parsedLogs and return the result of distinct ip
+     * @param {Log[]} parsedLogs - The parsedLogs to filter distinct ips
+     * @return {string[]} - Returns all logs filtered by selected http status code
+     */
+    public filterUniqueIps(parsedLogs: Log[]): string[] {
         let uniqueIps: string[] = [];
-        parsedLogs.map((logLine) => {
+        parsedLogs.forEach((logLine) => {
             if(!uniqueIps.includes(logLine.ip) && !this.ignoredIps.includes(logLine.ip)){
                 uniqueIps.push(logLine.ip);
             }
@@ -74,9 +86,15 @@ class LogParser {
         return uniqueIps;
     }
 
+    /**
+     * @desc - Filters the passed parsedLogs by http status code
+     * @param {Log[]} parsedLogs - The parsedLogs to search into
+     * @param {string} statusCode - The status code to search
+     * @return {Log[]} - Returns all logs filtered by selected http status code
+     */
     public getAllRequestsByStatusCode(parsedLogs: Log[], statusCode: string): Log[] {
         const requestsFilteredByStatusCode: Log[] = []
-        parsedLogs.map((log) => {
+        parsedLogs.forEach((log) => {
             if(log.statusCode === statusCode){
                 requestsFilteredByStatusCode.push(log);
             }
@@ -84,27 +102,40 @@ class LogParser {
         return requestsFilteredByStatusCode;
     }
 
-    public filterPotentialBotRequests(parsedLogs: Log[]){
-        let phpPathRequestLogs: Log[] = []
+    /**
+     * @desc Filters the passed parsedLogs
+     * @param {Log[]} parsedLogs The parsedLogs to search into for requests potentially made by bots
+     * @return {Log[]} Returns all logs which are made by bots
+    */
+    public filterPotentialBotRequests(parsedLogs: Log[]): Log[]{
+        let matchedBotLogs: Log[] = []
         if(this.forbiddenWords.length > 0){
-            parsedLogs.map((log) => {
+            parsedLogs.forEach((log) => {
                 if(this.forbiddenWords.every(word => log.path.includes(word)) && !this.ignoredIps.every(ip => log.path.includes(ip))){
-                    phpPathRequestLogs.push(log);
+                    matchedBotLogs.push(log);
                 }
             })
         }
-        return this.filterUniqueIps(phpPathRequestLogs);
+        return matchedBotLogs;
     }
 
-    /*
-    * @desc Export the IP Blacklist on JSON format
+    /**
+     * @desc Export in json format
+     * @param {string[]} blacklist The parsedLogs to search into for requests potentially made by bots
+     * @return {void}
      */
-    public exportBlacklist(){
-        fs.writeFileSync("./src/logs/ipBlacklist.json", JSON.stringify(this.blacklist));
+    public exportBlacklist(blacklist: string[]): void{
+        fs.writeFileSync("./src/logs/ipBlacklist.json", JSON.stringify(blacklist));
     }
 
-    public countStatusCode() {}
-    public readBlacklist(){}
+    /**
+     * @desc Export in json format
+     * @param {Log[]} parsedLogs The parsedLogs to search into for requests potentially made by bots
+     * @return {void}
+     */
+    public exportParsedLogFile(parsedLogs: Log[]): void {
+        fs.writeFileSync('src/logs/parsedLogs.json', JSON.stringify(parsedLogs, null, 2))
+    }
 }
 
 const logParser = new LogParser();
@@ -115,14 +146,17 @@ fs.readFile('src/logs/requests.log', {encoding: "utf8", flag: 'r'}, (err, file) 
     } else {
         const logLines = file.split('\n');
         const parsedLogs = logParser.parseLines(logLines);
+        logParser.exportParsedLogFile(parsedLogs);
         const notFoundRequests = logParser.getAllRequestsByStatusCode(parsedLogs, "404");
         const errorServerRequests = logParser.getAllRequestsByStatusCode(parsedLogs, "500");
         console.log("Total of requests :", parsedLogs.length);
         console.log("Total of error 500 :", errorServerRequests.length);
         console.log("Total of error 404 :", notFoundRequests.length);
         console.log("Total count of unique ip :", logParser.countDistinctIps(parsedLogs));
-        console.log("Total of blacklisted ip :", logParser.filterPotentialBotRequests(parsedLogs).length);
-        logParser.filterPotentialBotRequests(parsedLogs);
+        const matchedBotLogs = logParser.filterPotentialBotRequests(parsedLogs);
+        const ipsToBlacklist = logParser.filterUniqueIps(matchedBotLogs);
+        console.log("Total of blacklisted ip :", ipsToBlacklist.length);
+        logParser.exportBlacklist(ipsToBlacklist);
     }
 });
 
